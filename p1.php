58b2be450abdd8a25a77a90ea333fd099848fdf6
Vijus08 <vic.vijus@gmail.com>
2020-12-04 00:00:26 -0600

    phpPrac1

---
+++ p1.php
@@ -0,0 +1,11 @@
+<?php
+
+	echo "<h1> Estamos empezando a programar php...</h1>";
+	for ($i=1;$i<10;$i++){
+		echo "valor de numero $i <br>";
+	}
+
+	for (Si=1;$i<10;$i++){
+		echo '<input type="text" name="txtc'.$i.'"id="txtc'.$i.'"placeholder="casilla numero'.$i.'"><br>';
+	}
+?>
